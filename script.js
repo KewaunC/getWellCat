df57f898528ec735357ddc4c6f13ded3e43076eb
Kewaun Cain <kewaunscain@gmail.com>
2024-02-18 14:44:30 -0500

    get well card for a good guy

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('nightModeButton').addEventListener('click', toggleNightMode);
+
+function toggleNightMode() {
+  const body = document.body;
+  body.classList.toggle('night-mode');
+  button.classList.toggle('night-mode')
+}
